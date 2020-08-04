5d7991308e2871952460364d7a5cac32dfa42326
yukopratama <yukofland@gmail.com>
2020-08-04 09:39:20 +0800

    Latihan Row

---
+++ android/app/src/main/kotlin/com/example/row_column_widget_4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.row_column_widget_4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan Row dan Column"),
+        ),
+        body: Column(
+          children: <Widget>[Text("Text 1")],
+        ),
+      ),
+    );
+  }
+}
